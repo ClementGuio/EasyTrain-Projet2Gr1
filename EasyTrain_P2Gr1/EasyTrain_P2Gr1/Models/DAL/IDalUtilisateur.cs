5a38da3b8bd876e86286f95d89d35ec68566ae3a
PetitBrownie <jeanleo.bernard@gmail.com>
2023-10-24 10:20:45 +0200

    Ajout CRUD client IDAL DAL

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/DAL/IDalUtilisateur.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/DAL/IDalUtilisateur.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 
 namespace EasyTrain_P2Gr1.Models.DAL
 {
+
+
     public interface IDalUtilisateur : IDisposable
     {
+
+        // ---------------------------------------------- Gestion client
+
+        public int CreateClient(string nom, string prenom, DateTime dateDeNaissance, string adresseMail,
+                                string motDePasse, DateTime dateAbonnement);
         List<Client> GetClients();
+        public Client GetClient(int Id);
+        public void UpdateClient (int id, string nom, string prenom, DateTime dateDeNaissance, string adresseMail, 
+                                  string motDePasse, DateTime dateCreationDeCompte , DateTime dateAbonnement);
+        public void DeleteClient(int id);
+
     }
+
+
+
 }
