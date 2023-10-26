5e36516048b1c06497caa2bbf9f7a7be3e8a32ef
Bival2 <biaz2009@live.fr>
2023-10-26 13:38:51 +0200

    crud vue client

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/UtilisateurController.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/UtilisateurController.cs
@@ -3,6 +3,7 @@ using EasyTrain_P2Gr1.Models.Services;
 using EasyTrain_P2Gr1.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System;
 
 namespace EasyTrain_P2Gr1.Controllers
 {
@@ -37,5 +38,80 @@ namespace EasyTrain_P2Gr1.Controllers
             }
             return View("Error");
         }
+        [HttpGet]
+        public IActionResult CreerClient() 
+        {
+            return  View();
+        }
+        
+        [HttpPost]
+        public IActionResult CreerClient(Client client)
+        {
+            client.DateCreationCompte = DateTime.Now;
+            client.DateAbonnement = DateTime.Now;
+            using (IDalClient service = new ClientService())
+            {
+                service.CreateClient(client);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ModifierClient(int id) 
+        { 
+
+            if(id != 0)
+            {
+                Client client;
+                using(IDalClient service = new ClientService())
+                {
+                    client = service.GetClient(id);
+
+                }
+              if (client != null)
+                {
+                    return View(client);
+                }
+              
+            }
+            return View("Error") ;
+        }
+        [HttpPost]
+        public IActionResult ModifierClient(Client client)
+        {
+            using(IDalClient service = new ClientService())
+            {
+                service.UpdateClient(client);    
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SupprimerClient(int id)
+        {
+            if(id != 0)
+            {
+                Client client;
+                using(IDalClient service = new ClientService())
+                {
+                    client = service.GetClient(id);
+                }
+               if(client != null)
+                {
+                    return View(client);
+                }
+                
+            }
+            return View("Error");
+        }
+        [HttpPost]
+        public IActionResult SupprimerClient(Client client)
+        {
+            using( IDalClient service = new ClientService())
+            {
+                service.DeleteClient(client.Id);
+            }
+            return View();     
+        }
+    
     }
 }
