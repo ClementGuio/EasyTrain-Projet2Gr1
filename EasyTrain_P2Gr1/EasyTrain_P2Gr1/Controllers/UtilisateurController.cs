d195a35c4f8605d80fe738a62c22609ee20d92ce
PetitBrownie <jeanleo.bernard@gmail.com>
2023-10-26 13:42:30 +0200

    views crud gestionnaire

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/UtilisateurController.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/UtilisateurController.cs
@@ -37,5 +37,120 @@ namespace EasyTrain_P2Gr1.Controllers
             }
             return View("Error");
         }
+
+
+        //----------------------------------------- Controllers CRUD Gestionnaire
+
+        public IActionResult ListeGestionnaire() // Le nom de la méthode doit avoir le même nom que la vue
+        {
+            List<Gestionnaire> listeGestionnaire;
+            using (IDalGestionnaire service = new GestionnaireService())
+            {
+                listeGestionnaire = service.GetGestionnaires();
+            }
+            return View(listeGestionnaire);
+        }
+
+        public IActionResult AfficherProfilGestionnaire(int id)
+        {
+            if (id != 0)
+            {
+                Gestionnaire gestionnaire;
+                using (IDalGestionnaire service = new GestionnaireService())
+                {
+                    gestionnaire = service.GetGestionnaire(id);
+
+                }
+                if (gestionnaire != null)
+                {
+                    return View(gestionnaire);
+                }
+            }
+            return View("Error");
+        }
+
+        [HttpGet]
+        public IActionResult CreerGestionnaire() // Vue de création de profil GET
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreerGestionnaire(Gestionnaire gestionnaire) // Vue de création de profil Post
+        {
+            using (IDalGestionnaire dal = new GestionnaireService())
+            {
+                dal.CreateGestionnaire(gestionnaire);
+            }
+
+            return View(gestionnaire);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult ModifierProfilGestionnaire(int id) // Vue de modification de profil GET
+        {
+            if (id != 0)
+            {
+            Gestionnaire gestionnaire;
+            using (IDalGestionnaire service = new GestionnaireService())
+            {
+                gestionnaire = service.GetGestionnaire(id);
+
+            }
+            if (gestionnaire != null)
+            {
+                return View(gestionnaire);
+            }
+            }
+            return View("Error");
+
+        }
+
+        [HttpPost]
+        public IActionResult ModifierProfilGestionnaire(Gestionnaire gestionnaire) // Vue de modification de profil Post
+        {
+            using (IDalGestionnaire dal = new GestionnaireService())
+            {
+                dal.UpdateGestionnaire(gestionnaire);
+            }
+
+            return View(gestionnaire);
+
+        }
+
+        [HttpGet]
+        public IActionResult SupprimerGestionnaire(int id) // Vue de suppression de profil GET
+        {
+            if (id != 0)
+            { 
+            Gestionnaire gestionnaire;
+            using (IDalGestionnaire service = new GestionnaireService())
+            {
+                gestionnaire = service.GetGestionnaire(id);
+
+            }
+            if (gestionnaire != null)
+            {
+                return View(gestionnaire);
+            }
+            }
+            return View("Error");
+
+        }
+
+        [HttpPost]
+        public IActionResult SupprimerGestionnaire(Gestionnaire gestionnaire) // Vue de suppression de profil Post
+        {
+            using (IDalGestionnaire dal = new GestionnaireService())
+            {
+                dal.DeleteGestionnaire(gestionnaire.Id);
+            }
+
+            return View();
+
+        }
+        //----------------------------------------- Fin Controllers CRUD Gestionnaire //-------------------------------------
+
     }
 }
