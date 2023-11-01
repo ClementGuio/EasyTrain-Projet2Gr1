f3357a36c8e4e912ece249249636c68562309a6e
Sabrine SADEQ <votreemail@exemple.fr>
2023-11-01 17:55:56 +0100

    css supprimer coach

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/Utilisateur.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/Utilisateur.cs
@@ -17,6 +17,7 @@ namespace EasyTrain_P2Gr1.Models
         [Required(ErrorMessage = "Le prénom doit être renseigné.")]
         [MaxLength(30, ErrorMessage = "Le prénom ne doit pas excéder 30 caractères")]
         [RegularExpression("[a-zA-Z]+", ErrorMessage = "Le prénom ne doit contenir que des lettres")]
+        [Display(Name ="Prénom")]
         public string Prenom { get; set; }
 
         [Required(ErrorMessage = "La date de naissance doit être renseignée.")]
@@ -32,13 +33,15 @@ namespace EasyTrain_P2Gr1.Models
 
         [Required(ErrorMessage = "Le mot de passe doit être renseigné.")]
         [RegularExpression("^(?=.{8,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*$", ErrorMessage = "Le mot de passe doit avoir au moins 8 caractères et contenir 1 minuscule, 1 majuscule et 1 chiffre.")]
-        [Display(Name = "Mot de passe")]
+        [Display(Name = " Mot de passe")]
         public string MotDePasse { get; set; }
 
         //[Required(ErrorMessage = "La vérification du mot de passe doit être renseignée.")]
         [Compare("MotDePasse", ErrorMessage = "Les mots de passe ne correspondent pas.")]// Permet de comparer les deux champs et retourne un message d'erreur
+        [Display(Name = " Confirmer le nouveau mot de passe")]
         public string VerifMotDePasse { get; set; }
 
+        [Display(Name = "Date de création de compte ")]
         public DateTime DateCreationCompte { get; set; }
 
         public DateTime? DeletedAt { get; set; } // Permet de désactiver le compte sans le supprimer -> Si DeletedAt est null le compte est actif, sinon le compte a été supprimé (désactivé)
