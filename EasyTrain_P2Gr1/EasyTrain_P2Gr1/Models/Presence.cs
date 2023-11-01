d0299465d017df0f972665bdbe9eaf6ac71accb8
ClementGuio <clementguio@gmail.com>
2023-11-01 13:46:22 +0100

    En train de trier et réparer les vues

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/Presence.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Models/Presence.cs
@@ -7,12 +7,10 @@ namespace EasyTrain_P2Gr1.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public DateTime HeureArrivee { get; set; }
 
         public DateTime HeureDepart { get; set; }
 
-        [Required]
         public Client Client { get; set; }
 
     }
