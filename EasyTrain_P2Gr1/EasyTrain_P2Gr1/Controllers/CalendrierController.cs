59c625856ca00c363e5b0273eabb94c663590257
PetitBrownie <jeanleo.bernard@gmail.com>
2023-11-06 13:57:22 +0300

    lundi 13h57

---
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/CalendrierController.cs
@@ -0,0 +1,23 @@
+﻿using EasyTrain_P2Gr1.Models.Services.Interfaces;
+using EasyTrain_P2Gr1.Models.Services;
+using EasyTrain_P2Gr1.Models;
+using EasyTrain_P2Gr1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EasyTrain_P2Gr1.Controllers
+{
+    public class CalendrierController : Controller
+    {
+        public IActionResult CalendrierUtilisateur()
+        {
+            List<CoursProgramme> listeCoursProgrammes;
+            using (IDalCoursProgramme service = new CoursProgrammeService())
+            {
+                listeCoursProgrammes = service.GetCoursProgrammesAVenir();
+            }
+            CalendrierViewModel cvm = new CalendrierViewModel(listeCoursProgrammes, 30);
+            return View(cvm);
+        }
+    }
+}
