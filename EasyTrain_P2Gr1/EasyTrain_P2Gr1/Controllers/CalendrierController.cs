48cd4674be0558bd923b8347fb69da4c2642acef
PetitBrownie <jeanleo.bernard@gmail.com>
2023-11-06 18:15:47 +0300

    lundi 18h15

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/CalendrierController.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/CalendrierController.cs
@@ -4,11 +4,15 @@ using EasyTrain_P2Gr1.Models;
 using EasyTrain_P2Gr1.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using EasyTrain_P2Gr1.Models.DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
 
 namespace EasyTrain_P2Gr1.Controllers
 {
+
     public class CalendrierController : Controller
     {
+        [HttpGet]
         public IActionResult CalendrierUtilisateur()
         {
             List<CoursProgramme> listeCoursProgrammes;
@@ -18,6 +22,47 @@ namespace EasyTrain_P2Gr1.Controllers
             }
             CalendrierViewModel cvm = new CalendrierViewModel(listeCoursProgrammes, 30);
             return View(cvm);
+        }
+
+
+        public IActionResult CalendrierReservation(int id)
+        {
+
+            CoursProgramme coursProgramme;
+            using (IDalCoursProgramme service = new CoursProgrammeService())
+            {
+                coursProgramme = service.GetCoursProgramme(id);
+            }
+            Client client;
+            using (IDalClient service = new ClientService())
+            {
+                client = service.GetClient(HttpContext.User.Identity.Name);
+            }
+            Reservation reservation = new Reservation
+            {
+                CoursProgramme = coursProgramme,
+                Client = client
+            };
+
+            if (coursProgramme.PlacesLibres > 0)
+            {
+                coursProgramme.PlacesLibres--;
+                using (IDalReservation service = new ReservationService())
+                {
+                    service.CreateReservation(reservation);
+                }
+                using (IDalCoursProgramme service = new CoursProgrammeService())
+                {
+                    service.UpdateCoursProgramme(coursProgramme);
+                }
+                return RedirectToAction("Index", "Reservation");
+            }
+            return View ("CalendrierUtilisateur");
+
+
+
+            
+
         }
     }
 }
