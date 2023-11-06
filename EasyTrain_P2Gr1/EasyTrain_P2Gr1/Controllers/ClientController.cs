0b88047f8f21c33fe738f8ba34ffef7038287195
PetitBrownie <jeanleo.bernard@gmail.com>
2023-11-06 12:24:50 +0300

    lundi 12h24

--- EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/ClientController.cs
+++ EasyTrain_P2Gr1/EasyTrain_P2Gr1/Controllers/ClientController.cs
@@ -92,6 +92,18 @@ namespace EasyTrain_P2Gr1.Controllers
         [HttpGet]
         public IActionResult ModifierClient()
         {
+            if (HttpContext.User.IsInRole("Client"))
+            {
+                ViewData["role"] = "Client";
+            }
+            else if (HttpContext.User.IsInRole("Coach"))
+            {
+                ViewData["role"] = "Coach";
+            }
+            else if (HttpContext.User.IsInRole("Gestionnaire"))
+            {
+                ViewData["role"] = "Gestionnaire";
+            }
             string id = HttpContext.User.Identity.Name;
 
             Client client;
@@ -113,6 +125,19 @@ namespace EasyTrain_P2Gr1.Controllers
         [HttpPost]
         public IActionResult ModifierClient(Client client)
         {
+            if (HttpContext.User.IsInRole("Client"))
+            {
+                ViewData["role"] = "Client";
+            }
+            else if (HttpContext.User.IsInRole("Coach"))
+            {
+                ViewData["role"] = "Coach";
+            }
+            else if (HttpContext.User.IsInRole("Gestionnaire"))
+            {
+                ViewData["role"] = "Gestionnaire";
+            }
+
             if (!ModelState.IsValid)
             {
                 return View(client);
